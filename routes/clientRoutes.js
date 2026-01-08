aa5902dbf36867dd18073f7356637c50f2bb953a
guy-biton <guy171@gmail.com>
2026-01-08 20:15:56 +0200

    Cleanup: Removed redundant root files. Project is now strictly split.

--- routes/clientRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/clientRoutes.js
-const express = require('express');
-const router = express.Router();
-const clientController = require('../controllers/clientController');
-
-router.get('/', clientController.getClients);
-router.post('/add', clientController.addClientVulnerable); // We just need one add for Stored XSS, usually.
-
-module.exports = router;
